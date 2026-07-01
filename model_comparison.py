75b04f71f1e418ce25c75372c998da7aea2d5c63
arobotdinosaur <160431417+arobotdinosaur@users.noreply.github.com>
2026-06-30 23:50:34 -0700

    three new annotations, plus model comparison

--- model_comparison.py
+++ model_comparison.py
@@ -32,7 +32,7 @@ MODELS = [
     ("blackbox_clean","./models/clean_blackbox.keras", "unet"),
 ]
 
-IMAGE_DIR = "./test_only_noisy_images/"
+IMAGE_DIR = "./test_only_clean_images/"
 PATCH_DIR = "./model_comparison_workspace"
 OUTPUT_DIR = "./model_comparison_metrics"   # where per-model CSV files are saved
 SHOW_PLOTS = True
